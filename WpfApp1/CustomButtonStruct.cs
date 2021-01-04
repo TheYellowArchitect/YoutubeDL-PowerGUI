09e7a00387cb725c3d14da2a9c364d8980244a26
TheYellowArchitect <jimakos_mal@hotmail.com>
2021-01-04 12:16:45 -0800

    Original Upload

---
+++ WpfApp1/CustomButtonStruct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public struct CustomButtonStruct
+    {
+        public HorizontalAlignment buttonHorizontalAlignment;//Center
+        public VerticalAlignment buttonVerticalAlignment;//Top
+        public Thickness buttonMargin;//20,0,0,0
+        public double buttonWidth;//75
+        public string buttonStringContent;//Add Custom
+
+        public StackPanel buttonParent;
+    }
+}
---
+++ WpfApp1/QueryCommandFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class QueryCommandFilter
+    {
+        public Dictionary<int, string> queryCommandDictionary = new Dictionary<int, string>();
+    }
+}
